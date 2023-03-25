1a08ad8f2bb323ee133c4afb8abdea81d7f5a80e
@hexlet/cli <support@hexlet.io>
2023-03-25 14:57:26 +0300

    submit java-oop-ru/interfaces

--- java-oop-ru/interfaces/src/main/java/exercise/App.java
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 // BEGIN
-public static List buildApartmentsList(List<Home> apartments, int n) {
-    List sortedApps = apartments.stream()
-        .sorted(x -> x.getArea())
-        .limit(n)
-        .collect(Collectors.toList());
-    return sortedApps;
+public class App {
+    public static List buildApartmentsList(List<Home> apartments, int n) {
+        List sortedApps = apartments.stream()
+                .sorted(x -> x.getArea())
+                .limit(n)
+                .collect(Collectors.toList());
+        return sortedApps;
+    }
 }
+
 // END
