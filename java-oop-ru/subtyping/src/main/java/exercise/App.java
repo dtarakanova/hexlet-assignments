49fab51eeb6575e0ce9bfcd3b7ff9cf6995b5b46
@hexlet/cli <support@hexlet.io>
2023-03-26 08:07:15 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -5,10 +5,12 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 // BEGIN
-public static Map<String, String> swapKeyValue(Map<String, String> baseData) {
+public class App {
+    public static Map<String, String> swapKeyValue(Map<String, String> baseData) {
         Map<String, String> swapped = new HashMap<String, String>();
         for(Map.Entry<String, String> entry : baseData.entrySet())
-        swapped.put(entry.getValue(), entry.getKey());
+            swapped.put(entry.getValue(), entry.getKey());
         return swapped;
-        }
+    }
+}
 // END
