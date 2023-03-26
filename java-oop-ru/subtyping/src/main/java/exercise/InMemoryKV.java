2e0d4b0a24f1e61fb2cd95c052a360a8767a0ad9
@hexlet/cli <support@hexlet.io>
2023-03-26 07:44:14 +0300

    submit java-oop-ru/subtyping

---
+++ java-oop-ru/subtyping/src/main/java/exercise/InMemoryKV.java
@@ -0,0 +1,31 @@
+package exercise;
+
+import java.util.Map;
+import java.util.HashMap;
+
+// BEGIN
+public class InMemoryKV {
+    Map<String key, String value> baseData;
+
+    public InMemoryKV {
+        this.baseData = baseData;
+    }
+
+    public void set(String key, String value) {
+        baseData.put(key, value);
+    }
+
+    public void unset (String key) {
+        baseData.remove(key);
+    }
+
+    public String get(String key, String defaultValue) {
+        String getValue = baseData.getOrDefault(key, defaultValue);
+        return getValue;
+    }
+
+    public Map<String, String> toMap() {
+        return baseData.stream().collect(Collectors.toMap());
+    }
+}
+// END
