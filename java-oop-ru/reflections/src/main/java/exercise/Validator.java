32f4893aa7111c51b7a27a317a31104808066b33
@hexlet/cli <support@hexlet.io>
2023-04-10 11:07:48 +0300

    submit java-oop-ru/reflections

--- java-oop-ru/reflections/src/main/java/exercise/Validator.java
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -9,7 +9,7 @@ import static java.lang.System.out;
 public class Validator {
     public static List<String> validate(Address address) {
         List<String> allNulls = new ArrayList<>();
-        Field[] fields = address.getClass().getDeclaredFields()
+        Field[] fields = address.getClass().getDeclaredFields();
         for (Field field : fields) {
             if (field.getAnnotation(NotNull.class) == null) {
                 allNulls.add(String.valueOf(field));
