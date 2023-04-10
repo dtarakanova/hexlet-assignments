28094049d462cc36663170e899fd7c69e08857ab
@hexlet/cli <support@hexlet.io>
2023-04-10 10:14:28 +0300

    submit java-oop-ru/reflections

---
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -0,0 +1,18 @@
+package exercise;
+
+import java.lang.reflect.Field;
+// BEGIN
+public class Validator {
+    public static List<String> validate(Address address) {
+        List<String> allNulls = new ArrayList<>();
+        Field[] fields = address.Address().getDeclaredFields();
+        for (Field field : fields) {
+            RandomNumber randomNumber = field.getAnnotation(Address.class);
+            if (randomNumber == null) {
+                allNulls.add(field);
+            }
+        }
+        return allNulls;
+    }
+}
+// END
