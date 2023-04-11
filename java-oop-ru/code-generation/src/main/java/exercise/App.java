cbc2a0d8bb665b86c95d4d25381a164d145faea4
@hexlet/cli <support@hexlet.io>
2023-04-11 11:48:13 +0300

    submit java-oop-ru/code-generation

---
+++ java-oop-ru/code-generation/src/main/java/exercise/App.java
@@ -0,0 +1,29 @@
+package exercise;
+
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+
+// BEGIN
+class App {
+    public static void save(Path path, Car car) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            mapper.writeValue(new File(String.valueOf(path)), car);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Car extract(Path path) {
+        ObjectMapper mapper = new ObjectMapper();
+        Car car;
+        try {
+            car = mapper.readValue(new File(String.valueOf(path)), Car.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return car;
+    }
+}
+// END
---
+++ java-oop-ru/code-generation/src/main/java/exercise/Car.java
@@ -0,0 +1,40 @@
+package exercise;
+
+import lombok.Value;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+// BEGIN
+@Value
+// END
+class Car {
+    int id;
+    String brand;
+    String model;
+    String color;
+    User owner;
+
+    // BEGIN
+    public String serialize() {
+        ObjectMapper objectmapper = new ObjectMapper();
+        String jsonString= "";
+        try {
+            jsonString = mapper.writeValueAsString(toString());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return jsonToString;
+    }
+
+    public static Car unserialize(String jsonToString) {
+        ObjectMapper mapper = new ObjectMapper();
+        Car car;
+        try {
+            car = mapper.readValue(jsonString, Car.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return car;
+    }
+    // END
+}
