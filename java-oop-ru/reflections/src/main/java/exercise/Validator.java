b09d879b72bee2966abeacb540699ea3ab0731a2
@hexlet/cli <support@hexlet.io>
2023-04-12 10:11:52 +0300

    submit java-oop-ru/reflections

--- java-oop-ru/reflections/src/main/java/exercise/Validator.java
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -11,7 +11,7 @@ public class Validator {
         List<String> hasAnnotations = new ArrayList<>();
         List<String> allNulls = new ArrayList<>();
         Field[] fields = address.getClass().getDeclaredFields();
-        hasAnnotations = stream.of(fields)
+        hasAnnotations = Stream.of(fields)
                 .filter(field -> field.isAnnotationPresent(NotNull.class))
                 .collect(Collectors.toList());
         for (Field nullFields : hasAnnotations) {
